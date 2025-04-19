f9da22b1be7af0e02b60f817b5762c5353fff579
Biraj Kharel <birajkharel00@gmail.com>
2025-04-19 14:02:15 +0545

    Minor Hamburger adjustment

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -25,31 +24,3 @@ function toggleMenu() {
      }
    });
  });
-=======
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-
- // Get the filter dropdown and project cards
- const filterType = document.getElementById("filterType");
- const cards = document.querySelectorAll(".details-container");
-
- // Add event listener to the filter
- filterType.addEventListener("change", function() {
-   const selected = this.value;
-
-   // Loop through all the cards and display/hide based on the selected value
-   cards.forEach(card => {
-     const type = card.getAttribute("data-type");
-     
-     if (!selected || type === selected) {
-       card.style.display = "flex";  // or "block" depending on your layout
-     } else {
-       card.style.display = "none";
-     }
-   });
- });
->>>>>>> 84b539f (Addition of the Gallery section)
